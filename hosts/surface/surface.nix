7408723c9cb1d35cba578a0510be76aa61296470
Brian Rieth <rieth@me.com>
2026-05-11 07:58:30 +0200

    TLP: Akku-Charge-Limits raus, lädt jetzt bis 100%

--- hosts/surface/surface.nix
+++ hosts/surface/surface.nix
@@ -21,8 +21,7 @@
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      START_CHARGE_THRESH_BAT0 = 75;
-      STOP_CHARGE_THRESH_BAT0 = 90;
+      # Akku lädt bis 100% (vorher 75-90% Akku-Schon, hat Brian gestört)
       PLATFORM_PROFILE_ON_AC = "performance";
       PLATFORM_PROFILE_ON_BAT = "low-power";
     };
