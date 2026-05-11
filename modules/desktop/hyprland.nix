697fc5c207735330efbca1c330f1b8fbb78c00aa
Brian Rieth <rieth@me.com>
2026-05-11 13:40:40 +0200

    EMERGENCY: tuigreet wieder --cmd Hyprland (uwsm-Setup ist broken), Warning egal

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -17,7 +17,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --asterisks --cmd 'uwsm start Hyprland'";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --asterisks --cmd Hyprland";
         user = "greeter";
       };
     };
