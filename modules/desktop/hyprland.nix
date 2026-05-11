e4cf2c24e578c1e3ba544918e695aaaffec95731
Brian Rieth <rieth@me.com>
2026-05-11 13:34:30 +0200

    tuigreet --cmd 'uwsm start Hyprland' (Executable-Name statt .desktop)

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -17,7 +17,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --asterisks --cmd Hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --asterisks --cmd 'uwsm start Hyprland'";
         user = "greeter";
       };
     };
